051f1cb0bbad3cf74bde03c7f5a95e4021ff4a5e
정상필의GRAM\mod45 <mod459@naver.com>
2025-08-11 18:04:07 +0900

    회원가입 페이지 화면

---
+++ src/main/java/com/shop/shop/common/ViewApiController.java
@@ -0,0 +1,15 @@
+package com.shop.shop.common;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class ViewApiController {
+
+    @GetMapping("/auth/signup")
+    public String signupPage(){
+        return "auth/signup";
+    }
+}
